1e07ee223e9d6aaf21d5ede3be46d42804f69207
rubsuadav <rsuarezdavid@gmail.com>
2024-05-15 19:44:58 +0200

    chore: reverted bad PR

--- cypress/e2e/app.cy.jsx
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe("rendering App component", () => {
-  it("should render the app", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get("h1").contains("Mundo");
-  });
-});
