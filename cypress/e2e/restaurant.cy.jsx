47781f4ecf27ebebce0b146c3090e69261c1a770
rubsuadav <rsuarezdavid@gmail.com>
2024-05-16 14:09:39 +0200

    feat: improve E2E testing for restaurant menu page

--- cypress/e2e/restaurant.cy.jsx
+++ cypress/e2e/restaurant.cy.jsx
@@ -1,14 +1,21 @@
 /// <reference types="cypress" />
 
+let restaurantId;
+
 describe("showing the restaurant/menu component", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/restaurant");
+
+    // Interceptamos la solicitud GET y extraemos el ID del restaurante de la URL
+    cy.intercept("GET", /\/api\/restaurant\/(.*)/).as("getRestaurant");
   });
 
   it("should render the app", () => {
-    cy.intercept("GET", `/api/restaurant/3bdGxrc3e1yFzHcVGw5Y`).as(
-      "getRestaurant"
-    );
+    cy.wait("@getRestaurant").then((interception) => {
+      // Extraemos el ID del restaurante de la URL
+      const url = new URL(interception.request.url);
+      restaurantId = url.pathname.split("/")[3];
+    });
 
     cy.get("h2").should("exist");
     cy.get("button").should("exist");
@@ -20,15 +27,13 @@ describe("showing the restaurant/menu component", () => {
 
     cy.intercept(
       "GET",
-      `/api/restaurant/3bdGxrc3e1yFzHcVGw5Y/zUKq6KT3LRmYAe2yLOCR/showSections`
-    )
-      .as("getSections")
-      .then(() => {
-        cy.get("button").should("exist");
-        cy.get("h2").should("exist");
-        cy.get("span").should("exist");
-        cy.get("div").should("exist");
-        cy.get("p").should("exist");
-      });
+      `/api/restaurant/${restaurantId}/zUKq6KT3LRmYAe2yLOCR/showSections`
+    ).as("getSections");
+
+    cy.get("button").should("exist");
+    cy.get("h2").should("exist");
+    cy.get("span").should("exist");
+    cy.get("div").should("exist");
+    cy.get("p").should("exist");
   });
 });
