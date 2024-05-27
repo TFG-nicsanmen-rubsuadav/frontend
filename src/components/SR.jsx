cb675782a698a8a5b52ffcbb135b57dfcce4a271
rubsuadav <rsuarezdavid@gmail.com>
2024-05-27 21:09:30 +0200

    fix: Remove unused imports in SR.jsx and main.jsx

--- src/components/SR.jsx
+++ src/components/SR.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowUturnRightIcon, StarIcon } from "@heroicons/react/24/outline";
 
