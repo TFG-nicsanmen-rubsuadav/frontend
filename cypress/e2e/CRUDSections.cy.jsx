60626a0f55af4041aa2ffa220e5e99859095516c
rubsuadav <rsuarezdavid@gmail.com>
2024-06-04 01:38:37 +0200

    feat: Add force option to button clicks in CRUDSections.cy.jsx
    The force option has been added to the button clicks in the CRUDSections.cy.jsx file to ensure that the clicks are performed even if the elements are not fully visible on the page. This change improves the reliability of the section creation and update tests.

--- cypress/e2e/CRUDSections.cy.jsx
+++ cypress/e2e/CRUDSections.cy.jsx
@@ -26,22 +26,22 @@ const loginAndSetup = () => {
 describe("testing section creation", () => {
   it("can create sections", () => {
     loginAndSetup();
-    cy.get("button").eq(7).click();
+    cy.get("button", { timeout: 10000 }).eq(7).click();
     // close modal
-    cy.get("button").eq(8).click({ force: true });
+    cy.get("button").eq(8).click({ force: true, multiple: true });
     // open modal
-    cy.get("button").eq(7).click();
+    cy.get("button").eq(7).click({ force: true });
     type('input[name="name"]', "Section 1");
     type('input[name="description"]', "Description 1");
     cy.get('button[type="submit"]').click();
     cy.wait(2000);
     // click on the section
-    cy.get("button").eq(7).click();
+    cy.get("button").eq(7).click({ force: true });
     cy.scrollTo("bottom");
   });
   it("can't create sections with invalid data", () => {
     loginAndSetup();
-    cy.get("button").eq(8).click({ force: true });
+    cy.get("button").eq(8).click({ force: true, multiple: true });
     cy.get('button[type="submit"]').click();
     cy.wait(1000);
   });
@@ -52,7 +52,7 @@ describe("testing section update", () => {
     loginAndSetup();
     cy.wait(2000);
     // navigate to the section
-    cy.get("button").eq(7).click();
+    cy.get("button").eq(7).click({ force: true });
     cy.scrollTo("bottom");
     // open the section modal
     cy.get("button").eq(6).click();
